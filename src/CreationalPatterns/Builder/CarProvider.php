20359a0d5b8c4ebeb82932450213a7cd551288cc
Mohamed Karam <mohamed.karam@id3m.com>
2025-06-17 00:10:00 +0300

    Add anther exmple for creating init new car object

--- src/CreationalPatterns/Builder/CarProvider.php
+++ src/CreationalPatterns/Builder/CarProvider.php
@@ -3,12 +3,22 @@
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use MohamedKaram\DesignPatterns\CreationalPatterns\Builder\Models\Car;
+use MohamedKaram\DesignPatterns\CreationalPatterns\Builder\BMWCarBuilder;
 use MohamedKaram\DesignPatterns\CreationalPatterns\Builder\BENZCarBuilder;
 use MohamedKaram\DesignPatterns\CreationalPatterns\Builder\CarBuilderInterface;
 
 class CarProvider
 {
-    public function construct(CarBuilderInterface $builder): Car
+    public function constructSportsCar(CarBuilderInterface $builder): Car
+    {
+        $builder->createCar();
+        $builder->addBody();
+        $builder->addEngine();
+        $builder->addWhool();
+        return $builder->getCar();
+    }
+
+    public function constructLaxureCar(CarBuilderInterface $builder): Car
     {
         $builder->createCar();
         $builder->addBody();
@@ -19,6 +29,12 @@ class CarProvider
     }
 }
 
-$builder = new BENZCarBuilder();
+$bmwBuilder = new BMWCarBuilder();
+$benzBuilder = new BENZCarBuilder();
+
 $director = new CarProvider();
-$director->construct($builder);
+
+$bmwCar = $director->constructSportsCar($bmwBuilder);
+$benzCar = $director->constructLaxureCar($benzBuilder);
+
+var_dump($bmwCar, $benzCar);
