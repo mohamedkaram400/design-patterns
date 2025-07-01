7ce9624671c358775d040d12b94416c65314c956
Mohamed Karam <mohamedkaramm500@gmail.com>
2025-07-01 22:51:58 +0300

    Write unit test for payment exmple in adapter pattern

--- UnitTests/AdapterTest.php
+++ UnitTests/AdapterTest.php
@@ -1,10 +1,14 @@
 <?php
 
-use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Adaptee\TurboEngine;
-use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Car;
-use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Engines\NormalEngine;
-use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\TurboEngineAdapter;
 use PHPUnit\Framework\TestCase;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Car;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\PaymentExmple\Payment;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\TurboEngineAdapter;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Adaptee\TurboEngine;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\CarExmple\Engines\NormalEngine;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\PaymentExmple\OldPaymentAdapter;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\PaymentExmple\Adaptee\OldPayment;
+use MohamedKaram\DesignPatterns\StructuralPatterns\Adapter\PaymentExmple\Payments\PaypalPayment;
 
 class AdapterTest extends TestCase
 {
@@ -21,5 +25,20 @@ class AdapterTest extends TestCase
         $adapter = new TurboEngineAdapter($engine);
         $car = new Car($adapter);
         $this->assertEquals('Start engine from turbo car', $car->startEngine());
+    }
+
+    public function testCanPaypalPaymentEngine()
+    {
+        $paypal = new PaypalPayment();
+        $payment = new Payment($paypal);
+        $this->assertEquals('from PaypalPayment payment class', $payment->payment());
+    }
+
+    public function testCanMakeOldPayemntEngine()
+    {
+        $engine = new OldPayment();
+        $adapter = new OldPaymentAdapter($engine);
+        $payment = new Payment($adapter);
+        $this->assertEquals('from old payment class', $payment->payment());
     }
 }
